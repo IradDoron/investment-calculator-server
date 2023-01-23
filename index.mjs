a51357fc6d75e5945f138bb289d4f3175b860f80
Irad Doron <irad.doron@gmail.com>
2023-01-23 17:37:46 +0200

    empty backend

--- index.mjs
+++ index.mjs
@@ -15,10 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(function (req, res, next) {
-	res.header(
-		'Access-Control-Allow-Origin',
-		'https://investment-calculator-irad-matan.netlify.app/'
-	);
+	res.header('Access-Control-Allow-Origin', '*');
 	res.header(
 		'Access-Control-Allow-Headers',
 		'Origin, X-Requested-With, Content-Type, Accept'
@@ -30,91 +27,91 @@ const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST || 'localhost';
 
 app.get('/', (req, result) => {
-	result.end;
+	result.end('Hello World!');
 });
 
-app.post('/', (req, response) => {
-	const { body } = req;
-	const {
-		timeOfInvestment,
-		stockTicket,
-		initialInvestment,
-		monthlyContribution,
-	} = body;
+// app.post('/', (req, response) => {
+// 	const { body } = req;
+// 	const {
+// 		timeOfInvestment,
+// 		stockTicket,
+// 		initialInvestment,
+// 		monthlyContribution,
+// 	} = body;
 
-	const options = {
-		args: [
-			timeOfInvestment,
-			stockTicket,
-			initialInvestment,
-			monthlyContribution,
-		],
-	};
+// 	const options = {
+// 		args: [
+// 			timeOfInvestment,
+// 			stockTicket,
+// 			initialInvestment,
+// 			monthlyContribution,
+// 		],
+// 	};
 
-	PythonShell.run('main.py', options, (err, res) => {
-		if (err) {
-			console.log(err);
-		}
+// 	PythonShell.run('main.py', options, (err, res) => {
+// 		if (err) {
+// 			console.log(err);
+// 		}
 
-		let formattedResObject = '';
-		res.forEach((item) => {
-			formattedResObject += item;
-			formattedResObject += '\n';
-		});
+// 		let formattedResObject = '';
+// 		res.forEach((item) => {
+// 			formattedResObject += item;
+// 			formattedResObject += '\n';
+// 		});
 
-		const jsonsList = formattedResObject.split('&*&');
+// 		const jsonsList = formattedResObject.split('&*&');
 
-		const parsedJsonsList = jsonsList.map((item) => JSON.parse(item));
+// 		const parsedJsonsList = jsonsList.map((item) => JSON.parse(item));
 
-		const graphDict = parsedJsonsList[0];
-		const divGraphDict = parsedJsonsList[1];
-		const divYearYield = parsedJsonsList[2];
+// 		const graphDict = parsedJsonsList[0];
+// 		const divGraphDict = parsedJsonsList[1];
+// 		const divYearYield = parsedJsonsList[2];
 
-		const formattedResoultObject = {
-			graphDict,
-			divGraphDict,
-			divYearYield,
-		};
+// 		const formattedResoultObject = {
+// 			graphDict,
+// 			divGraphDict,
+// 			divYearYield,
+// 		};
 
-		const stringifyedResoultObject = JSON.stringify(formattedResoultObject);
+// 		const stringifyedResoultObject = JSON.stringify(formattedResoultObject);
 
-		response.setHeader('Access-Control-Allow-Origin', '*');
-		response.setHeader('Access-Control-Allow-Credentials', 'true');
-		response.setHeader(
-			'Access-Control-Allow-Methods',
-			'GET,HEAD,OPTIONS,POST,PUT'
-		);
-		response.setHeader(
-			'Access-Control-Allow-Headers',
-			'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
-		);
+// 		response.setHeader('Access-Control-Allow-Origin', '*');
+// 		response.setHeader('Access-Control-Allow-Credentials', 'true');
+// 		response.setHeader(
+// 			'Access-Control-Allow-Methods',
+// 			'GET,HEAD,OPTIONS,POST,PUT'
+// 		);
+// 		response.setHeader(
+// 			'Access-Control-Allow-Headers',
+// 			'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
+// 		);
 
-		response.setHeader(
-			'Access-Control-Allow-Headers',
-			'Content-Type, Authorization'
-		);
+// 		response.setHeader(
+// 			'Access-Control-Allow-Headers',
+// 			'Content-Type, Authorization'
+// 		);
 
-		response.send(stringifyedResoultObject);
+// 		response.send(stringifyedResoultObject);
 
-		// const formattedResObject = {
-		// 	value_of_today: res[0],
-		// 	total_contribution: res[1],
-		// 	cumulative_interest: res[2],
-		// 	average_annual_interest_of_final_value: res[3],
-		// 	average_annual_interest_of_ticket: res[4],
-		// };
+// 		// const formattedResObject = {
+// 		// 	value_of_today: res[0],
+// 		// 	total_contribution: res[1],
+// 		// 	cumulative_interest: res[2],
+// 		// 	average_annual_interest_of_final_value: res[3],
+// 		// 	average_annual_interest_of_ticket: res[4],
+// 		// };
 
-		// 		const formattedResoultString = `
-		// Final value of today: ${formattedResObject.value_of_today}
-		// total contribution: ${formattedResObject.total_contribution}
-		// Cumulative interest of ${formattedResObject.cumulative_interest}%
-		// Average annual interest of final value: ${formattedResObject.average_annual_interest_of_final_value}%
-		// Average annual interest of ticket: ${formattedResObject.average_annual_interest_of_ticket}%
-		//         `;
+// 		// 		const formattedResoultString = `
+// 		// Final value of today: ${formattedResObject.value_of_today}
+// 		// total contribution: ${formattedResObject.total_contribution}
+// 		// Cumulative interest of ${formattedResObject.cumulative_interest}%
+// 		// Average annual interest of final value: ${formattedResObject.average_annual_interest_of_final_value}%
+// 		// Average annual interest of ticket: ${formattedResObject.average_annual_interest_of_ticket}%
+// 		//         `;
 
-		// console.log(formattedResoultString);
-	});
-});
+// 		// console.log(formattedResoultString);
+// 	});
+// });
 
 app.listen(PORT, HOST, () => {
 	console.log(`Server running at http://${HOST}:${PORT}...`);
